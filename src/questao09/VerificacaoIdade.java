f62c038d8de71794cdec85f89a3882cb87dee4ae
Rubem Chaves <108533543+vinicchaves@users.noreply.github.com>
2023-08-08 00:48:22 -0300

    Update VerificacaoIdade.java

--- src/questao09/VerificacaoIdade.java
+++ src/questao09/VerificacaoIdade.java
@@ -1,1 +1,18 @@
-package questao09;import java.util.Scanner;public class VerificacaoIdade {    public static void main(String[] args) {        Scanner entrada = new Scanner(System.in);        System.out.print("Digite a sua idade: ");        int idade = entrada.nextInt();        if (idade >= 18) {            System.out.println("Voc� � maior de idade!");        } else {            System.out.println("Voc� � menor de idade!");        }    }}
+package questao09;
+
+import java.util.Scanner;
+
+public class VerificacaoIdade {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade!");
+        } else {
+            System.out.println("Você é menor de idade!");
+        }
+    }
+}
